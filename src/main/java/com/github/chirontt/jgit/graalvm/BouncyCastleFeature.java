cfcb43f3b59cbf82a2110a8ae53d6ee66f19c885
Tue Ton <49886739+chirontt@users.noreply.github.com>
2022-09-15 00:02:41 -0400

    fix native-image compile errors
    as GraalVM 22.2.0's native-image program now runs on the module path
    by default.

--- src/main/java/com/github/chirontt/jgit/graalvm/BouncyCastleFeature.java
+++ src/main/java/com/github/chirontt/jgit/graalvm/BouncyCastleFeature.java
@@ -1,20 +1,16 @@
 package com.github.chirontt.jgit.graalvm;
 
-import org.bouncycastle.crypto.CryptoServicesRegistrar;
-import org.bouncycastle.jcajce.provider.drbg.DRBG;
+import java.security.Security;
+
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.graalvm.nativeimage.ImageSingletons;
 import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeClassInitialization;
-import org.graalvm.nativeimage.impl.RuntimeClassInitializationSupport;
 
 import com.oracle.svm.core.annotate.AutomaticFeature;
 
-import java.security.Security;
-
 /**
  * derived from
- * https://github.com/micronaut-projects/micronaut-oracle-cloud/blob/master/oraclecloud-sdk/src/main/java/io/micronaut/oraclecloud/clients/BouncyCastleFeature.java
+ * https://github.com/micronaut-projects/micronaut-oracle-cloud/pull/17
  */
 @AutomaticFeature
 public class BouncyCastleFeature implements Feature {
@@ -25,23 +21,6 @@ public class BouncyCastleFeature implements Feature {
         RuntimeClassInitialization.initializeAtBuildTime("org.bouncycastle");
         //...in order to register its security provider
         Security.addProvider(new BouncyCastleProvider());
-    }
-
-    @Override
-    public void duringSetup(DuringSetupAccess access) {
-        //programmatically re-run class initialization at runtime
-        //(equivalent to the (deprecated) --rerun-class-initialization-at-runtime=
-        //command-line option for GraalVM's native-image command)
-        RuntimeClassInitializationSupport rci = ImageSingletons.lookup(RuntimeClassInitializationSupport.class);
-        //all org.bouncycastle packages are initialized at build time,
-        //but some specific classes need be re-initialized at runtime
-        //due to static SecureRandom seeding
-        rci.rerunInitialization(CryptoServicesRegistrar.class,
-                                "See https://github.com/micronaut-projects/micronaut-oracle-cloud/pull/17#discussion_r472955378");
-        rci.rerunInitialization(DRBG.Default.class,
-                                "See https://github.com/micronaut-projects/micronaut-oracle-cloud/pull/17#discussion_r472955378");
-        rci.rerunInitialization(DRBG.NonceAndIV.class,
-                                "See https://github.com/micronaut-projects/micronaut-oracle-cloud/pull/17#discussion_r472955378");
     }
 
 }
